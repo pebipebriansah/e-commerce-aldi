4e4dd804200b1d57f80a633b7c1fb43aa328860d
Pebi Pebriansah <50487966+pebipebriansah@users.noreply.github.com>
2025-01-23 11:44:13 +0700

    Update profile.php

--- app/Views/customer/profile/profile.php
+++ app/Views/customer/profile/profile.php
@@ -79,7 +79,7 @@
                             </div>
                             <div class="mb-3">
                                 <label for="mySelect2" class="form-label">Alamat</label>
-                                <input type="text" class="form-control" id="mySelect2" name="alamat">
+                                <select id="mySelect2" class="form-select" name="alamat"></select>
                             </div>
                             <div class="mb-3">
                                 <label for="subject" class="form-label">Password Baru</label>
